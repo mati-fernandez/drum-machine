43bab7021b6ba5ae7155d5d6baf3df06b946c754
Matias Fernandez <matifer.dev@gmail.com>
2024-03-15 17:29:20 -0300

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,66 @@ import './App.css';
 const drumPads = [
   {
     id: 'Q',
-    src: 'assets/Heater-1.mp3',
+    src: '/src/assets/Heater-1.mp3',
   },
   {
     id: 'W',
-    src: 'assets/Heater-2.mp3',
+    src: '/src/assets/Heater-2.mp3',
   },
   {
     id: 'E',
-    src: 'assets/Heater-3.mp3',
+    src: '/src/assets/Heater-3.mp3',
   },
   {
     id: 'A',
-    src: 'assets/Heater-4.mp3',
+    src: '/src/assets/Heater-4_1.mp3',
   },
   {
     id: 'S',
-    src: 'assetsHeater-6.mp3',
+    src: '/src/assets/Heater-6.mp3',
   },
   {
     id: 'D',
-    src: 'assets/Dsc_Oh.mp3',
+    src: '/src/assets/Dsc_Oh.mp3',
   },
   {
     id: 'Z',
-    src: 'assets/Kick_n_Hat.mp3',
+    src: '/src/assets/Kick_n_Hat.mp3',
   },
   {
     id: 'X',
-    src: 'assets/RP4_KICK_1.mp3',
+    src: '/src/assets/RP4_KICK_1.mp3',
   },
   {
     id: 'C',
-    src: 'assets/Cev_H2.mp3',
+    src: '/src/assets/Cev_H2.mp3',
   },
 ];
+
+const playAudio = (id) => {
+  const audio = document.getElementById(id);
+  audio.play();
+  console.log(audio);
+};
 
 function App() {
   return (
     <div id="drum-machine">
       <div id="display"></div>
-      <div className="drum-pads">
-        {drumPads.map((drumPad) => {
-          <button className="drum-pad" id={drumPad.id}>
+      <div id="drum-pads">
+        {drumPads.map((drumPad) => (
+          <button
+            key={drumPad.id}
+            className="drum-pad"
+            id={drumPad.src}
+            onClick={() => {
+              playAudio(drumPad.id);
+            }}
+          >
             {drumPad.id}
-          </button>;
-        })}
+            <audio className="clip" id={drumPad.id} src={drumPad.src}></audio>
+          </button>
+        ))}
       </div>
     </div>
   );
